6e5375a7fe3ce28293a898306e441762c06f42f1
Axojhf <20625305+axojhf@users.noreply.github.com>
2023-11-03 20:39:13 +0800

    fix

---
+++ mod.ps1
@@ -0,0 +1,16 @@
+# 文件路径
+$file = "C:\Clasharp\Clasharp\Clasharp.csproj"
+
+# 读取文件内容
+$content = Get-Content $file
+
+# 插入 <PublishAot>true</PublishAot> 到第十一行换行之后
+$lineNumber = 11
+$lineToInsert = "<PublishAot>true</PublishAot>"
+$content = $content -replace "(^.*\r\n){$($lineNumber - 1)}", "`$&$lineToInsert`r`n"
+
+# 修改 <TargetFramework>net7.0</TargetFramework> 中的 7.0 为 8.0
+$content = $content -replace "<TargetFramework>net7\.0</TargetFramework>", "<TargetFramework>net8.0</TargetFramework>"
+
+# 写回文件
+Set-Content -Path $file -Value $content
